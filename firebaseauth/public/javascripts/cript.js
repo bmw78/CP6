9054ff1ae528b82e0ce816e0c0ea269aaf586899
EC2 Default User <ec2-user@ip-172-31-28-164.us-east-2.compute.internal>
2018-12-11 01:14:40 +0000

    adding

--- firebaseauth/public/javascripts/cript.js
+++ firebaseauth/public/javascripts/cript.js
@@ -1,7 +1,7 @@
 var myApp = angular.module("gif", ["firebase"]);
 myApp.controller("MainCtrl", ["$scope", "$firebaseArray",
     function($scope, $firebaseArray) {
-        var ref = firebase.database().ref().child("messages");
+        var ref = firebase.database().ref().child("gifs");
         $scope.chats = $firebaseArray(ref);
         $scope.update = function(user) {
             var newmessage = { from: user.name || "anonymous", body: user.chat };
