de5c8022090cd42ec7ad9151ecbd9c9d815336ed
James A <james.ashdown@byu.net>
2018-12-11 00:10:13 +0000

    changed to new firebase project

---
+++ firebaseauth/public/javascripts/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyARZA4D4dIVsNl7b-GHSxEmRWHeGYo4bDA",
+	authDomain: "creative6-288a3.firebaseapp.com",
+	databaseURL: "https://creative6-288a3.firebaseio.com",
+	projectId: "creative6-288a3",
+	storageBucket: "creative6-288a3.appspot.com",
+	messagingSenderId: "662795861231"
+};
+firebase.initializeApp(config);
